8435a222a875ee4a783f49beb9aa587b01a9c868
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-18 16:31:06 +0530

    DataBase

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,18 +1,26 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+
 const db = mysql.createConnection({
-    host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    port: 4000,
-    user: "2P5o9dEST1LyBui.root",
-    password: k64VzyYHPAMTnRLi,
-    database: "sophynova",
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectTimeout: 30000
+    host: process.env.DB_HOST || "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    user: process.env.DB_USER || "2P5o9dEST1LyBui.root",
+    password: process.env.DB_PASSWORD || "k64VzyYHPAMTnRLi" ,
+    database: process.env.DB_NAME || "sophynova",
+    port: process.env.DB_PORT || 4000,
+    
+    ssl:{rejectUnauthorized: false}
+
 });
+
+
 db.connect((err) => {
     if (err) {
-        console.error(err);
+        console.log("❌ MySQL Connection Error:");
+        console.log(err);
     } else {
-        console.log("✅ Connected Successfully");
+        console.log("✅ MySQL Connected Successfully");
     }
 });
+
+module.exports = db;
