e13b1984ca1e2a0be19f9d9ab540afe67972dde1
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-18 16:12:31 +0530

    database Issues Not going Content in right DB

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,25 +1,18 @@
-const mysql = require("mysql2");
-require("dotenv").config();
-
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    user: process.env.DB_USER || "2P5o9dEST1LyBui.root",
-    password: process.env.DB_PASSWORD || "k64VzyYHPAMTnRLi",
-    database: process.env.DB_NAME || "sophynova",
-    port: process.env.DB_PORT || 4000,
-    
-    ssl:{rejectUnauthorized: false}
-
+    host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    port: 4000,
+    user: "2P5o9dEST1LyBui.root",
+    password: k64VzyYHPAMTnRLi,
+    database: "sophynova",
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectTimeout: 30000
 });
-
-
 db.connect((err) => {
     if (err) {
-        console.log("❌ MySQL Connection Error:");
-        console.log(err);
+        console.error(err);
     } else {
-        console.log("✅ MySQL Connected Successfully");
+        console.log("✅ Connected Successfully");
     }
 });
-
-module.exports = db;
