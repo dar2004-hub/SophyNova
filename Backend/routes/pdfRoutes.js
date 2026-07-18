8928c52f35e779a8adafe183cbf3bff22bfea4d3
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-18 23:09:28 +0530

    Routes

--- Backend/routes/pdfRoutes.js
+++ Backend/routes/pdfRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const multer = require("../middleware/upload");
+const multer = require("../Middleware/upload");
 
 const {
 
