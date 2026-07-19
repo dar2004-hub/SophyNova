4d5db62eb18288dc0794096b573e1c61929059e8
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-20 00:11:32 +0530

    Update MockTestSection.jsx

--- src/components/MockTestSection.jsx
+++ src/components/MockTestSection.jsx
@@ -1,12 +1,1 @@
-function MockTestSection() {
-  return (
-    <section className="py-20 text-white text-center">
-      <h2 className="text-4xl font-bold">
-        Mock Test Portal
-      </h2>
 
-    </section>
-  );
-}
-
-export default MockTestSection;
