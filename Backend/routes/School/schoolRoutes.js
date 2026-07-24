1edb4f14bd1160bb7890398aed4d95a404152ca9
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-24 16:12:21 +0530

    Update

--- Backend/routes/School/schoolRoutes.js
+++ Backend/routes/School/schoolRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const upload = require("../../middleware/multer");
+const upload = require("../../Middleware/Multer");
 
 const {
     getClasses
