e9cdfebb5ab8e34a398aefae8357c4c07dea4c28
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-25 15:39:06 +0530

    Fetc

--- Backend/controllers/School/schoolUploadController.js
+++ Backend/controllers/School/schoolUploadController.js
@@ -1,5 +1,5 @@
 const supabase = require("../../config/supabase");
-const db = require("../config/db");
+const db = require("../../config/db");
 
 // ==========================================================
 // UPLOAD PDF
