87f1f6939812f5fdda9c6b4a0db13866c9ba2ca6
www/App.com <140878223+dar2004-hub@users.noreply.github.com>
2026-07-25 15:48:58 +0530

    New

--- Backend/routes/School/schoolRoutes.js
+++ Backend/routes/School/schoolRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const multer = require("../Middleware/Multer");
+const multer = require("../../Middleware/Multer");
 
 const {
 
